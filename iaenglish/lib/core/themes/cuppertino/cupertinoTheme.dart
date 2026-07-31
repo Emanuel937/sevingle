145cc1f50069ad879b7b82b5c96fff6cc0180cf8
emanuel937 <emanuelabizimi@gmail.com>
2026-07-31 02:34:32 +0100

    empting the main folder

--- iaenglish/lib/core/themes/cuppertino/cupertinoTheme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:iaenglish/core/themes/provider/themeProvide.dart';
-
-CupertinoThemeData setTheme({required ThemeProvider themeProvider}) {
-  return CupertinoThemeData(
-    scaffoldBackgroundColor: themeProvider.colors.scaffoldPageBackground,
-    brightness: themeProvider.isDark ? Brightness.dark : Brightness.light,
-  );
-}
